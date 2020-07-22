7ac036b3ee72738029f06418019840d7447a3077
Celeste <529453864@qq.com>
2020-07-22 21:28:11 +0800

    leetcode542题

---
+++ src/leetcode0720/Test.java
@@ -0,0 +1,14 @@
+package leetcode0720;
+
+/**
+ * @author: celeste
+ * @create: 2020-07-22 13:00
+ * @description:
+ **/
+public class Test {
+    public static void main(String[] args) {
+        int[][] matrix = {{0, 0, 0}, {0, 1, 0}, {1, 1, 1}};
+        UpdateMatrix u = new UpdateMatrix();
+        u.updateMatrix(matrix);
+    }
+}
